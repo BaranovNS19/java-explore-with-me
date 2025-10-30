4df302834b701e439231f2c786a666c46044d573
BaranovNS <baranovns@vtb.ru>
2025-10-31 01:55:48 +0300

    diplom part 1

--- product/src/main/java/practicum/ru/product/client/StatisticFeignClient.java
+++ product/src/main/java/practicum/ru/product/client/StatisticFeignClient.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import practicum.ru.product.dto.VisitGetResponseDto;
-import practicum.ru.product.dto.VisitPostRequestDto;
-import practicum.ru.product.dto.Visit;
+import practicum.ru.statistic.visit.Visit;
+import practicum.ru.statistic.visit.dto.VisitGetResponseDto;
+import practicum.ru.statistic.visit.dto.VisitPostRequestDto;
 
 
 import java.time.LocalDateTime;
