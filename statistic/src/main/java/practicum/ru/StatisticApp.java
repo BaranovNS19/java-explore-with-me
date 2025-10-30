8d4227ae97d84e1c24dd43e987915d1e2a0e08e6
BaranovNS <baranovns@vtb.ru>
2025-10-31 00:21:36 +0300

    diplom part 1

--- statistic/src/main/java/practicum/ru/StatisticApp.java
+++ statistic/src/main/java/practicum/ru/StatisticApp.java
@@ -5,7 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StatisticApp {
+    static {
+        System.out.println("=== STATIC INITIALIZER ===");
+        System.out.println("Java: " + System.getProperty("java.version"));
+        System.out.println("Classpath: " + System.getProperty("java.class.path"));
+        System.out.println("User Dir: " + System.getProperty("user.dir"));
+        System.out.println("=== END STATIC INITIALIZER ===");
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(StatisticApp.class, args);
+        System.out.println("=== MAIN METHOD STARTED ===");
+        try {
+            SpringApplication.run(StatisticApp.class, args);
+            System.out.println("=== SPRING STARTED SUCCESSFULLY ===");
+        } catch (Exception e) {
+            System.err.println("=== SPRING STARTUP FAILED ===");
+            e.printStackTrace();
+            throw e;
+        }
     }
 }
