e80d08b25d9eca8fcc3752e6fa6de69a2da3d893
BaranovNS <baranovns@vtb.ru>
2025-11-01 23:03:03 +0300

    diplom part 2

--- statistic/src/main/java/practicum/ru/statistic/visit/VisitController.java
+++ statistic/src/main/java/practicum/ru/statistic/visit/VisitController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.*;
 import practicum.ru.statistic.visit.dto.VisitGetResponseDto;
 import practicum.ru.statistic.visit.dto.VisitPostRequestDto;
 
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -33,8 +31,8 @@ public class VisitController {
                                                @RequestParam(required = false) List<String> uris,
                                                @RequestParam(required = false, defaultValue = "false") boolean unique) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        LocalDateTime startDate = LocalDateTime.parse(URLDecoder.decode(start, StandardCharsets.UTF_8), formatter);
-        LocalDateTime endDate = LocalDateTime.parse(URLDecoder.decode(end, StandardCharsets.UTF_8), formatter);
+        LocalDateTime startDate = LocalDateTime.parse(start, formatter);
+        LocalDateTime endDate = LocalDateTime.parse(end, formatter);
         return visitService.getVisits(startDate, endDate, uris, unique);
     }
 }
