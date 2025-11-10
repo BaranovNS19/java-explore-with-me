76d1784f18fdb6fb87c5002cf148290ee6e1cfd3
BaranovNS <baranovns@vtb.ru>
2025-11-11 00:02:38 +0300

    diplom part 2

--- product/src/main/java/practicum/ru/product/event/EventRepository.java
+++ product/src/main/java/practicum/ru/product/event/EventRepository.java
@@ -10,6 +10,26 @@ import java.util.List;
 public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> findByInitiatorId(Long initiatorId);
 
+//    @Query("SELECT e FROM Event e WHERE " +
+//            "(:text IS NULL OR LOWER(e.annotation) LIKE LOWER(CONCAT('%', :text, '%')) " +
+//            "OR LOWER(e.description) LIKE LOWER(CONCAT('%', :text, '%'))) AND " +
+//            "(:categories IS NULL OR e.category.id IN :categories) AND " +
+//            "(:paid IS NULL OR e.paid = :paid) AND " +
+//            "(:rangeStart IS NULL OR e.eventDate >= :rangeStart) AND " +
+//            "(:rangeEnd IS NULL OR e.eventDate <= :rangeEnd) AND " +
+//            "(:onlyAvailable = false OR e.confirmedRequests < e.participantLimit) AND " +
+//            "e.state = practicum.ru.product.event.Status.PUBLISHED " +  // Используем enum напрямую
+//            "ORDER BY " +
+//            "CASE WHEN :sort = 'VIEWS' THEN e.views END DESC, " +
+//            "CASE WHEN :sort != 'VIEWS' THEN e.eventDate END ASC")
+//    List<Event> findEvents(@Param("text") String text,
+//                           @Param("categories") List<Long> categories,
+//                           @Param("paid") Boolean paid,
+//                           @Param("rangeStart") LocalDateTime rangeStart,
+//                           @Param("rangeEnd") LocalDateTime rangeEnd,
+//                           @Param("onlyAvailable") Boolean onlyAvailable,
+//                           @Param("sort") String sort);
+
     @Query("SELECT e FROM Event e WHERE " +
             "(:text IS NULL OR LOWER(e.annotation) LIKE LOWER(CONCAT('%', :text, '%')) " +
             "OR LOWER(e.description) LIKE LOWER(CONCAT('%', :text, '%'))) AND " +
@@ -18,10 +38,8 @@ public interface EventRepository extends JpaRepository<Event, Long> {
             "(:rangeStart IS NULL OR e.eventDate >= :rangeStart) AND " +
             "(:rangeEnd IS NULL OR e.eventDate <= :rangeEnd) AND " +
             "(:onlyAvailable = false OR e.confirmedRequests < e.participantLimit) AND " +
-            "e.state = practicum.ru.product.event.Status.PUBLISHED " +  // Используем enum напрямую
-            "ORDER BY " +
-            "CASE WHEN :sort = 'VIEWS' THEN e.views END DESC, " +
-            "CASE WHEN :sort != 'VIEWS' THEN e.eventDate END ASC")
+            "e.state = 'PUBLISHED' AND " +
+            "(:sort IS NULL OR 1=1)")  // ← ФИКТИВНОЕ ИСПОЛЬЗОВАНИЕ sort
     List<Event> findEvents(@Param("text") String text,
                            @Param("categories") List<Long> categories,
                            @Param("paid") Boolean paid,
