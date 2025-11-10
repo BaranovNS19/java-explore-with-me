59e9eee9b0ced7f208a72c0075de3388308a9416
BaranovNS <baranovns@vtb.ru>
2025-11-11 01:32:17 +0300

    diplom part 2

--- product/src/main/java/practicum/ru/product/event/EventRepository.java
+++ product/src/main/java/practicum/ru/product/event/EventRepository.java
@@ -2,9 +2,7 @@ package practicum.ru.product.event;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
